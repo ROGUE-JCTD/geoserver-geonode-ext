2ffa9ec4d5332335c925a8a972b6daf52de6d4d6
Syrus Mesdaghi <syrusm@gmail.com>
2015-09-03 18:40:29 -0600

    added build script to gather all the right branches and repos and build in the right order

---
+++ build/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# exit if anything returns failure
+set -e
+
+rm -rf temp-build-dir
+mkdir temp-build-dir
+cd temp-build-dir
+REPOS_DIR=`pwd`
+
+git clone -b mapstory-2.6.x https://github.com/MapStory/geoserver-geonode-ext.git
+cd geoserver-geonode-ext
+# only run clean, do not run install yet
+mvn clean -DskipTests
+cd ${REPOS_DIR}
+
+# adds support for xdate needed for importing layers with time attrib
+git clone -b postgis-xdate-udt-12.x https://github.com/MapStory/geotools.git
+cd geotools 
+mvn install -DskipTests
+cd ${REPOS_DIR}
+
+# gets latest geogig to fix a transaction end bug. check for nightly builds can drop this if up-to-date
+git clone -b master https://github.com/locationtech/geogig.git
+cd geogig/src/parent
+mvn install -DskipTests
+cd ${REPOS_DIR}
+
+# has a date fix which allows importing layers with time attrib into geogig
+git clone -b 2.6.x https://github.com/garnertb/geoserver.git
+cd geoserver/src
+mvn install -DskipTests
+cd ${REPOS_DIR}
+
+# build latest 2.6.x geogig rest api, no nightly snapshots available
+git clone -b 2.6.x https://github.com/boundlessgeo/geoserver-exts.git
+cd geoserver-exts
+mvn install -DskipTests
+cd ${REPOS_DIR}
+
+cd geoserver-geonode-ext
+mvn install -DskipTests
+mvn war:war
+
