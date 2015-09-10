7c666929ddc3bff22f44c209ac6f1c6aa2fae156
Syrus Mesdaghi <syrusm@gmail.com>
2015-09-10 16:27:17 -0400

    Update build.sh
    build with only specific geoserver-ext/geogig, no geoserver, or geogig

--- build/build.sh
+++ build/build.sh
@@ -14,27 +14,27 @@ mvn clean -DskipTests
 cd ${REPOS_DIR}
 
 # adds support for xdate needed for importing layers with time attrib
-git clone -b postgis-xdate-udt-12.x https://github.com/rogue-jctd/geotools.git
-cd geotools 
-mvn install -DskipTests
-cd ${REPOS_DIR}
+#git clone -b postgis-xdate-udt-12.x https://github.com/rogue-jctd/geotools.git
+#cd geotools 
+#mvn install -DskipTests
+#cd ${REPOS_DIR}
 
 # gets latest geogig to fix a transaction end bug. check for nightly builds can drop this if up-to-date
-git clone -b master https://github.com/locationtech/geogig.git
-cd geogig/src/parent
-mvn install -DskipTests
-cd ${REPOS_DIR}
+#git clone -b master https://github.com/locationtech/geogig.git
+#cd geogig/src/parent
+#mvn install -DskipTests
+#cd ${REPOS_DIR}
 
 # has a date fix which allows importing layers with time attrib into geogig
-git clone -b 2.6.x https://github.com/rogue-jctd/geoserver.git
-cd geoserver/src/extension/importer
-mvn install -DskipTests
-cd ${REPOS_DIR}
+#git clone -b 2.6.x https://github.com/rogue-jctd/geoserver.git
+#cd geoserver/src/extension/importer
+#mvn install -DskipTests
+#cd ${REPOS_DIR}
 
 # build latest 2.6.x geogig rest api, no nightly snapshots available. 
 # pull it in from rogue since needed a build fix for jline
 git clone -b 2.6.x https://github.com/rogue-jctd/geoserver-exts.git
-cd geoserver-exts
+cd geoserver-exts/geogig
 mvn install -DskipTests
 cd ${REPOS_DIR}
 
